505b805944fb385dc3ec282b2ebe43309eb0ff1e
Alexandro <105998036+Eternalred05@users.noreply.github.com>
2026-05-14 22:25:57 -0400

    Fixed Table allowing to change cells by loading the Table model again

--- src/GUI/ListadoPacientes.java
+++ src/GUI/ListadoPacientes.java
@@ -37,7 +37,6 @@ public class ListadoPacientes extends javax.swing.JDialog {
 
         tablaPacientes.getTableHeader().setReorderingAllowed(false);
 
-        tablaPacientes.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
     }
 
     private void cargarHospitales() {
@@ -96,9 +95,8 @@ public class ListadoPacientes extends javax.swing.JDialog {
     }
 
     private void llenarTabla(ArrayList<PacienteListado> lista) {
-        String[] columnas = {"Hospital", "Departamento", "Unidad", "Nº Hist. Clínica",
-            "Nombre", "Fecha Nac.", "Dirección"};
-        DefaultTableModel model = new DefaultTableModel(columnas, 0);
+        DefaultTableModel model = (DefaultTableModel) tablaPacientes.getModel();
+        model.setRowCount(0);
         for (PacienteListado p : lista) {
             model.addRow(new Object[]{
                 p.getHospital(),
@@ -110,7 +108,7 @@ public class ListadoPacientes extends javax.swing.JDialog {
                 p.getDireccion()
             });
         }
-        tablaPacientes.setModel(model);
+
     }
 
     @SuppressWarnings("unchecked")
@@ -126,6 +124,8 @@ public class ListadoPacientes extends javax.swing.JDialog {
         jButton1 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         tablaPacientes = new javax.swing.JTable();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -161,6 +161,15 @@ public class ListadoPacientes extends javax.swing.JDialog {
         ));
         jScrollPane1.setViewportView(tablaPacientes);
 
+        jButton2.setText("Dar de Baja");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setText("Modificar");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -181,10 +190,15 @@ public class ListadoPacientes extends javax.swing.JDialog {
                         .addComponent(comboDepartamento, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(47, 47, 47)
                         .addComponent(comboUnidad, javax.swing.GroupLayout.PREFERRED_SIZE, 194, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(123, 123, 123))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jButton1)
-                        .addContainerGap())))
+                        .addGap(123, 123, 123))))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(225, 225, 225)
+                .addComponent(jButton2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton3)
+                .addGap(138, 138, 138)
+                .addComponent(jButton1)
+                .addContainerGap())
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(layout.createSequentialGroup()
                     .addContainerGap()
@@ -204,9 +218,16 @@ public class ListadoPacientes extends javax.swing.JDialog {
                     .addComponent(comboHospital, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(comboUnidad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(comboDepartamento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 464, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addContainerGap())
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 456, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jButton2)
+                            .addComponent(jButton3))
+                        .addGap(19, 19, 19))))
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                     .addContainerGap(160, Short.MAX_VALUE)
@@ -233,6 +254,10 @@ public class ListadoPacientes extends javax.swing.JDialog {
             JOptionPane.showMessageDialog(this, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -281,6 +306,8 @@ public class ListadoPacientes extends javax.swing.JDialog {
     private javax.swing.JComboBox<Object> comboHospital;
     private javax.swing.JComboBox<Object> comboUnidad;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
