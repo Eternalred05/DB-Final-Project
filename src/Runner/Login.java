3cb3193b521582393c4272216077fe234a5cb63c
Alexandro <105998036+Eternalred05@users.noreply.github.com>
2026-05-19 04:21:02 -0400

    Changed awt checkbox for javax one, also changed some depracated components like absolute layout (However accessButton.addItemListener(...) may bring issues with these while compiling)

--- src/Runner/Login.java
+++ src/Runner/Login.java
@@ -18,8 +18,15 @@ public class Login extends javax.swing.JFrame {
      */
     public Login() {
         initComponents();
+       
         passwordField.setEchoChar('\u2022');
+        passCheck.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                passCheckActionPerformed(evt);
+            }
+        });
         updateButtonState();
+        configurarListeners();
         setLocationRelativeTo(null);
     }
 
@@ -27,59 +34,39 @@ public class Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel4 = new javax.swing.JLabel();
         accessButton = new javax.swing.JButton();
-        passwordField = new Utils.JPasswordFieldEdited();
-        passCheck = new java.awt.Checkbox();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         userField = new Utils.JTextFieldEdited();
-        iconLabel = new javax.swing.JLabel();
+        passwordField = new Utils.JPasswordFieldEdited();
+        passCheck = new javax.swing.JCheckBox();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Login");
-        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/LoginBG.jpg"))); // NOI18N
 
         accessButton.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         accessButton.setText("Acceder");
+        accessButton.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                accessButtonItemStateChanged(evt);
+            }
+        });
         accessButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 accessButtonActionPerformed(evt);
             }
         });
-        getContentPane().add(accessButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 380, 90, 40));
-
-        passwordField.setFont(new java.awt.Font("Tahoma", 0, 11)); // NOI18N
-        passwordField.setLimit(11);
-        passwordField.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                passwordFieldActionPerformed(evt);
-            }
-        });
-        passwordField.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyReleased(java.awt.event.KeyEvent evt) {
-                passwordFieldKeyReleased(evt);
-            }
-        });
-        getContentPane().add(passwordField, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 210, 217, -1));
-
-        passCheck.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        passCheck.setLabel("Mostrar");
-        passCheck.addItemListener(new java.awt.event.ItemListener() {
-            public void itemStateChanged(java.awt.event.ItemEvent evt) {
-                passCheckItemStateChanged(evt);
-            }
-        });
-        getContentPane().add(passCheck, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 250, -1, -1));
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 0));
         jLabel1.setText("Contraseña");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 190, -1, -1));
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 0));
         jLabel2.setText("Usuario");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 140, -1, -1));
 
         userField.setFont(new java.awt.Font("Tahoma", 0, 11)); // NOI18N
         userField.addActionListener(new java.awt.event.ActionListener() {
@@ -92,27 +79,68 @@ public class Login extends javax.swing.JFrame {
                 userFieldKeyReleased(evt);
             }
         });
-        getContentPane().add(userField, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 160, 217, -1));
 
-        iconLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/LoginBG.jpg"))); // NOI18N
-        getContentPane().add(iconLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+        passCheck.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        passCheck.setText("Mostrar");
+        passCheck.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                passCheckItemStateChanged(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(210, 210, 210)
+                .addComponent(passwordField, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(290, 290, 290)
+                .addComponent(jLabel1))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(300, 300, 300)
+                .addComponent(jLabel2))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(520, 520, 520)
+                .addComponent(accessButton, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(210, 210, 210)
+                .addComponent(userField, javax.swing.GroupLayout.PREFERRED_SIZE, 217, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(280, 280, 280)
+                .addComponent(passCheck))
+            .addComponent(jLabel4)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(220, 220, 220)
+                .addComponent(passwordField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(190, 190, 190)
+                .addComponent(jLabel1))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(140, 140, 140)
+                .addComponent(jLabel2))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(380, 380, 380)
+                .addComponent(accessButton, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(160, 160, 160)
+                .addComponent(userField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(247, 247, 247)
+                .addComponent(passCheck))
+            .addComponent(jLabel4)
+        );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void passwordFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_passwordFieldActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_passwordFieldActionPerformed
 
-    private void passCheckItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_passCheckItemStateChanged
-        if (evt.getStateChange() == ItemEvent.SELECTED) {
-            // Acción cuando se selecciona
-            passwordField.setEchoChar((char) 0);
-        } else {
-            // Acción cuando se deselecciona
-            passwordField.setEchoChar('\u2022');
-        }
-    }//GEN-LAST:event_passCheckItemStateChanged
+    }//GEN-LAST:event_passwordFieldActionPerformed
 
     private void userFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_userFieldActionPerformed
 
@@ -121,15 +149,14 @@ public class Login extends javax.swing.JFrame {
     private void accessButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_accessButtonActionPerformed
         String password = passwordField.getText();
         String user = userField.getText();
-        
+
         MainMenu menu = new MainMenu();
         menu.setVisible(true);
         this.dispose();
-        
-        
-        
+
 
     }//GEN-LAST:event_accessButtonActionPerformed
+
 
     private void userFieldKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_userFieldKeyReleased
         updateButtonState();
@@ -138,13 +165,33 @@ public class Login extends javax.swing.JFrame {
     private void passwordFieldKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_passwordFieldKeyReleased
         updateButtonState();
     }//GEN-LAST:event_passwordFieldKeyReleased
+
+    private void passCheckItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_passCheckItemStateChanged
+        passCheck.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                passCheckActionPerformed(evt);
+            }
+        });
+    }//GEN-LAST:event_passCheckItemStateChanged
+
+    private void accessButtonItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_accessButtonItemStateChanged
+        // TODO add your handling code here:
+    }//GEN-LAST:event_accessButtonItemStateChanged
     private void updateButtonState() {
         if (!userField.getText().isEmpty() && !passwordField.getText().isEmpty()) {
             accessButton.setEnabled(true);
         } else {
             accessButton.setEnabled(false);
         }
+    }
 
+    private void passCheckActionPerformed(java.awt.event.ActionEvent evt) {
+
+        if (passCheck.isSelected()) {
+            passwordField.setEchoChar((char) 0);
+        } else {
+            passwordField.setEchoChar('\u2022');
+        }
     }
 
     public static void main(String args[]) {
@@ -179,12 +226,42 @@ public class Login extends javax.swing.JFrame {
         });
     }
 
+    private void configurarListeners() {
+
+        userField.getDocument().addDocumentListener(new javax.swing.event.DocumentListener() {
+            public void changedUpdate(javax.swing.event.DocumentEvent e) {
+                updateButtonState();
+            }
+
+            public void removeUpdate(javax.swing.event.DocumentEvent e) {
+                updateButtonState();
+            }
+
+            public void insertUpdate(javax.swing.event.DocumentEvent e) {
+                updateButtonState();
+            }
+        });
+        passwordField.getDocument().addDocumentListener(new javax.swing.event.DocumentListener() {
+            public void changedUpdate(javax.swing.event.DocumentEvent e) {
+                updateButtonState();
+            }
+
+            public void removeUpdate(javax.swing.event.DocumentEvent e) {
+                updateButtonState();
+            }
+
+            public void insertUpdate(javax.swing.event.DocumentEvent e) {
+                updateButtonState();
+            }
+        });
+    }
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton accessButton;
-    private javax.swing.JLabel iconLabel;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private java.awt.Checkbox passCheck;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JCheckBox passCheck;
     private Utils.JPasswordFieldEdited passwordField;
     private Utils.JTextFieldEdited userField;
     // End of variables declaration//GEN-END:variables
