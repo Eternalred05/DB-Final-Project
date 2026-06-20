01b9fc985ab6070e2d275670677870bdd0eca77a
Alexandro <105998036+Eternalred05@users.noreply.github.com>
2026-06-20 00:51:00 -0400

    Added a KeyEvent to consume entered numbers

--- src/Utils/JTextFieldEdited.java
+++ src/Utils/JTextFieldEdited.java
@@ -1,42 +1,43 @@
-
 package Utils;
 
 import javax.swing.JTextField;
-
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.util.function.Consumer;
-
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import javax.swing.JTextField;
 
 public class JTextFieldEdited extends JTextField {
 
-	public JTextFieldEdited() {
-		addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyTyped(KeyEvent e) {
-				JTextField text = (JTextField) e.getSource();
-				if (text.getText().length() == limit)
-					e.consume();
-			}
+    private boolean numbers = true;
+    private int limit = -1;
 
-		});
+    public JTextFieldEdited() {
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                JTextField text = (JTextField) e.getSource();
+                char c = e.getKeyChar();
 
-	}
+                if (text.getText().length() == limit || (!isNumbers() && Character.isLetter(c))) {
+                    e.consume();
+                }
+            }
+        });
+    }
 
-	private int limit = -1;
+    public boolean isNumbers() {
+        return numbers;
+    }
 
-	public int getLimite() {
-		return this.limit;
-	}
-	public void setLimit(int limit) {
-		if (limit >= -1)
-			this.limit = limit;
-	}
+    public void setNumbers(boolean numbers) {
+        this.numbers = numbers;
+    }
 
+    public int getLimite() {
+        return limit;
+    }
 
+    public void setLimit(int limit) {
+        if (limit >= -1) {
+            this.limit = limit;
+        }
+    }
 }
-
-
